f8fc1543ef0306f9d6beb9b60bbbab756ade3599
Joseph Chacko <ousepachn@gmail.com>
2024-03-03 18:53:19 -0500

    updated the transcript to read from videos

--- buildtranscripts.py
+++ buildtranscripts.py
@@ -3,15 +3,20 @@ import pandas as pd
 import moviepy.editor as mp
 from openai import OpenAI
 from langdetect import detect
+import utils.openAiRateLimit as oirt
+import re
 # Define the paths
 audio_outfolder = "audio_output/"
 transcript_outfolder = "transcript_output/"
 
 # Function to convert video to audio
-def convert_video_to_audio(video_path, audio_outfolder):
+def convert_video_to_audio(video_path, audio_outfolder,creatorid):
     video_name = os.path.basename(video_path)
     audio_name = os.path.splitext(video_name)[0] + ".mp3"
-    audio_path = os.path.join(audio_outfolder, audio_name)
+    audio_path = os.path.join(audio_outfolder,creatorid, audio_name)
+    #Create folder for creator, videos if none exists
+    if not os.path.exists(os.path.join(audio_outfolder,creatorid)):
+        os.makedirs(os.path.join(audio_outfolder,creatorid))
     clip=mp.VideoFileClip(video_path)
     clip.audio.write_audiofile(audio_path)
     return audio_path
@@ -19,7 +24,7 @@ def convert_video_to_audio(video_path, audio_outfolder):
 # Function to convert audio to transcript
 def convert_audio_to_transcript(audio_path):
     audio_file= open(audio_path, "rb")
-    transcript = client.audio.transcriptions.create(
+    transcript = oirt.audiocompletions_with_backoff(
     model="whisper-1",
     file=audio_file,
     prompt=Prompt
@@ -29,7 +34,7 @@ def convert_audio_to_transcript(audio_path):
 # Append together pickle files and create a dataframe
 data = pd.DataFrame()
 for file in os.listdir():
-    if file.endswith(".pkl"):
+    if file.endswith("dataframe.pickle"):
         df = pd.read_pickle(file)
         data=pd.concat([data,df], ignore_index=True)
 
@@ -37,7 +42,8 @@ for file in os.listdir():
 for url in data["url"]:
     # 
     if url.endswith(".mp4"):
-        audio_path = convert_video_to_audio(url, audio_outfolder)
+        creatorid = df.loc[df["url"] == url,'creatorid'].values[0]
+        audio_path = convert_video_to_audio(url, audio_outfolder,creatorid)
         data.loc[data["url"] == url, "audio_url"] = audio_path
     
  
@@ -45,18 +51,18 @@ for url in data["url"]:
 client = OpenAI()
 Model="whisper-1"
 Prompt='Please add punctuation. Do not miss any words.  Even if the word is soft, please transcribe it.'
-for url in data["audio_url"][data["audio_transcript"].isna()]:
+for url in data["audio_url"]:
     if str(url)!='nan':
     # Convert audio to transcripts.  Iterate through the dataframe and process each audio URL
         transcript=convert_audio_to_transcript(url)
-        print(transcript)
+        print(url)
        # Save the transcript in the dataframe
         data.loc[data["audio_url"] == url, "audio_transcript"] = transcript.text# Save the dataframe as a pickle file
 
 # build only english version of the transcripts from audio
 #TODO - NEED TO SEE HOW TO REMOVE AUDIO LYRICS FRMO AUDIO
 for t in data["audio_transcript"]:
-    if str(t)!='nan' and len(t)>1:
+    if str(t)!='nan' and len(t)>1 and bool(re.match('^(?=.*[a-zA-Z])', t)):
         if detect(t) == 'en':
             data.loc[data["audio_transcript"] == t, "en_audio_transcript"]=t
 
@@ -64,3 +70,5 @@ data.drop_duplicates(subset=['post_id'],inplace=True)
 data.to_pickle("dataframe.pickle")
 
 data.to_csv("dataframe.csv")
+
+
--- createdata.py
+++ createdata.py
@@ -1,73 +1,97 @@
 import instaloader
 import pandas as pd
 import glob
-
-posts_data = []
-
-usernames=["whatsmitafound"]  ## (hydrationceo, whatsmitafound, mkbhd)
-u=usernames[0]
-ig=instaloader.Instaloader()
-pf=instaloader.Profile
-
-# Load a profile from an Instagram handle
-profile = pf.from_username(ig.context, u)
-print(profile.username)
-print('username:',profile.username)
-print('followers:',profile.followers)
-print('posts:',profile.mediacount)
-print('bio:', profile.biography)
-print('links:', profile.external_url) 
-pic=ig.download_profiles([profile],posts=False)
-posts = profile.get_posts()
+import os
 
 
-for i, p in enumerate(posts):
-    if i >= 50:
-        break
-    
-    posts_path = f'posts'
-    ig.download_post(p, f'{posts_path}')
-    
-    jpg_files = glob.glob(f'posts/*{str(p.date_utc)[:10]}_{str(p.date_utc)[11:12]}*.jpg')
-    vid_files = glob.glob(f'posts/*{str(p.date_utc)[:10]}_{str(p.date_utc)[11:12]}*.mp4')
-
-    if jpg_files:
-        thumbnail_url=jpg_files[0]
-    else:
-        thumbnail_url=None
-
-    if p.is_video:
-        url=vid_files[0]
-    else:
-        url=jpg_files[0]
-
-    post_data = {
+def getprofiledata(username):
+    profile = pf.from_username(ig.context, username)
+    profile_data = {
         "creatorid":  profile.username,
-        "post_id": p.shortcode,
-        "title": p.title,
-        "caption": p.caption,
-        "likes": p.likes,
-        "thumbnail_url": thumbnail_url,
-        "url": url,
-        "date_utc": p.date_utc,
-        "typename": p.typename,
-        "caption_hashtags": p.caption_hashtags,
-        "is_video": p.is_video
+        "followers": profile.followers,
+        "posts": profile.mediacount,
+        "bio": profile.biography,
+        "links": profile.external_url
     }
+    print(profile.username)
+    print('username:',profile.username)
+    print('followers:',profile.followers)
+    print('posts:',profile.mediacount)
+    print('bio:', profile.biography)
+    print('links:', profile.external_url) 
+    return profile_data
 
 
-    
+def getpostsdata(profile):
+    posts = profile.get_posts()
+    posts_data = []
+    for i, p in enumerate(posts):
+        if i >= 50:
+            break
+        folder_name=os.path.join('posts', profile.username )
+        #Create folder for creator, if none exists
+        if not os.path.exists(folder_name):
+                os.makedirs(folder_name)
+        ig.download_post(p, folder_name)
+        
+        jpg_files = glob.glob(f'posts/{influencer}/*{str(p.date_utc)[:10]}_{str(p.date_utc)[11:13]}-{str(p.date_utc)[14:16]}*.jpg')
+        vid_files = glob.glob(f'posts/{influencer}/*{str(p.date_utc)[:10]}_{str(p.date_utc)[11:13]}-{str(p.date_utc)[14:16]}*.mp4')
+        print(f'posts/{influencer}/*{str(p.date_utc)[:10]}_{str(p.date_utc)[11:13]}-{str(p.date_utc)[14:16]}*.jpg')
 
-    posts_data.append(post_data)
+        if jpg_files:
+            thumbnail_url=jpg_files[0]
+        else:
+            thumbnail_url=None
 
-df = pd.DataFrame(posts_data)
+        if p.is_video:
+            url=vid_files[0]
+        else:
+            url=jpg_files[0]
+        print('reading post:', i,p.shortcode)
+        post_data = {
+            "creatorid":  profile.username,
+            "post_id": p.shortcode,
+            "title": p.title,
+            "caption": p.caption,
+            "likes": p.likes,
+            "thumbnail_url": thumbnail_url,
+            "url": url,
+            "date_utc": p.date_utc,
+            "typename": p.typename,
+            "caption_hashtags": p.caption_hashtags,
+            "is_video": p.is_video
+        }
+        posts_data.append(post_data)
+    return posts_data
 
-df.to_pickle(f'all-posts.pkl')
-df.to_csv(f'all-posts.csv', index=False)
 
 
-# df.to_pickle(f'hydrationceo-posts.pkl')
-# df.to_csv(f'hydrationceo-posts.csv', index=False)
 
-   
+ig=instaloader.Instaloader(dirname_pattern="posts\\{profile}",save_metadata=False,compress_json=False)
+pf=instaloader.Profile  
+
+sa_posts_data = []
+sa_profiles_data = [] 
+
+for influencer in ['hydrationceo', 'whatsmitafound', 'mkbhd']:
+     print('S1 reading influencer:', influencer)
+     profile = pf.from_username(ig.context, influencer)
+     profile_data=getprofiledata(influencer)
+     sa_profiles_data.append(profile_data)
+     print('S2 updated profile data:', influencer)
+     print('S3 reading posts data:', influencer)
+     posts_data=getpostsdata(pf.from_username(ig.context, influencer))
+     sa_posts_data.append(posts_data)
+
+combined_posts_data = []
+for sublist in sa_posts_data:
+    combined_posts_data.extend(sublist)
+
+df_combined_posts = pd.DataFrame(combined_posts_data)
+df_profiles = pd.DataFrame(sa_profiles_data)
+
+df_combined_posts.to_pickle(f'all-posts-03022024.pkl')
+df_profiles.to_pickle(f'all-profiles03022024.pkl')
+
+df_combined_posts.to_csv(f'all-posts.csv', index=False)
 
